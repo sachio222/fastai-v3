fbf980912ced1223b562f461bfbdb860bd0434df
Jake Krajewski <sachio222@users.noreply.github.com>
2019-10-09 23:37:10 -0400

    Replaced url for model and matched class list.

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1OYze_qhPNTf2Q-k91x6uGJJmgI_W9LMH'
+export_file_name = 'colours_JSK.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['black', 'white', 'red', 'blue', 'green', 'yellow']
 path = Path(__file__).parent
 
 app = Starlette()
