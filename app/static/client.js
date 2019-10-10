388f2e593be2f76791adbd736ae2bb41d7a1c214
Jake Krajewski <sachio222@users.noreply.github.com>
2019-10-10 00:41:28 -0400

    Manually changed case of button text

--- app/static/client.js
+++ app/static/client.js
@@ -18,7 +18,7 @@ function analyze() {
   var uploadFiles = el("file-input").files;
   if (uploadFiles.length !== 1) alert("Please select a file to analyze!");
 
-  el("analyze-button").innerHTML = "Analyzing...";
+  el("analyze-button").innerHTML = "ANALYZING...";
   var xhr = new XMLHttpRequest();
   var loc = window.location;
   xhr.open("POST", `${loc.protocol}//${loc.hostname}:${loc.port}/analyze`,
@@ -31,7 +31,7 @@ function analyze() {
       var response = JSON.parse(e.target.responseText);
       el("result-label").innerHTML = `Result = ${response["result"]}`;
     }
-    el("analyze-button").innerHTML = "Analyze";
+    el("analyze-button").innerHTML = "ANALYZE";
   };
 
   var fileData = new FormData();
